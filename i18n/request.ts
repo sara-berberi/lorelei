3ce2b25c9c314d4283eb6d4976c26dc8d40a10d0
Sara Berberi <sberberi21@epoka.edu.al>
2026-08-11 07:01:18 +0200

    empty

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,5 +1,5 @@
-import { getRequestConfig } from 'next-intl/server';
-import { routing } from './routing';
+import { getRequestConfig } from "next-intl/server";
+import { routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale;
@@ -10,7 +10,6 @@ export default getRequestConfig(async ({ requestLocale }) => {
 
   return {
     locale,
-    messages: (await import(`./messages/${locale}.json`)).default
+    messages: (await import(`./messages/${locale}.json`)).default,
   };
 });
-
